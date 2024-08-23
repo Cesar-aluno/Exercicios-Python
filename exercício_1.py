a3c093ff7c7583c492fc3f0b462f9726c83e6927
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-08-23 20:38:53 -0300

    Salvando exercícios 1, 5 e 7

---
+++ exercício_1.py
@@ -0,0 +1,13 @@
+#1) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.
+A = 10
+B = 20
+print(f"o valor de A é {A}" )
+print(f"o valor de B é {B}")
+
+b = A
+A = B
+a = A
+
+print(f"o valor de a é {a}" )
+print(f"o valor de b é {b}")
+	
---
+++ exercício_5.py
@@ -0,0 +1,6 @@
+#5) Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor.
+
+escreva = int(input("Digite um valor: "))
+print("O valor digitado é:", escreva)
+antecessor = escreva -1
+print("o antecessor é:",antecessor)

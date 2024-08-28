d8b358102992cbd26a3f4a4d616a418a1595c0f1
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-08-28 20:24:51 -0300

    exercicio_15.py

---
+++ exercicio_15.py
@@ -0,0 +1,8 @@
+#15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+
+valor = int(input( "Digite um valor: "))
+if valor >=0:  
+     print ( "É positivo")
+else:
+    print( "É negativo")
+    

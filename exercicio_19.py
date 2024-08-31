2f6d64cce99ca9c65d208dfcacb1e18977878de6
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-08-30 21:19:09 -0300

    exercicio_19.py

---
+++ exercicio_19.py
@@ -0,0 +1,9 @@
+#19) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. 
+print( "Apresentando valores ! ")
+valor1 = int(input( "Apresentando o primeiro valor: "))
+valor2 = int(input( "Apresentando o segundo valor: "))
+
+if valor1 > valor2:
+    print ( "O maior valor é: ", valor1)
+else:
+    print ( "O maior valor é: ", valor2)

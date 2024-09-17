987c7e769fb011793c4e3e3a225c79b3d65acc42
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-09-17 18:41:31 -0300

    Create exercicio_30.py

---
+++ exercicio_30.py
@@ -0,0 +1,8 @@
+#30) Ler 3 valores (considere que não serão informados valores iguais) e escrevê-los em ordem crescente.
+
+
+valor1 = int(input(" Digite o primeiro valor: "))
+valor2 = int(input(" Digite o segundo valor: "))
+valor3 = int(input(" Digite o terceiro valor: "))
+
+print(valor3, valor2, valor1)

3c47848e69997882e8f9b8e7e5a0e1770ff57c97
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-10-04 19:05:17 -0300

    Create exercicio_35.py

---
+++ exercicio_35.py
@@ -0,0 +1,10 @@
+'''
+35) Um posto está vendendo combustíveis com a seguinte tabela de descontos: Álcool - até 20 litros, desconto de 3% por litro
+    acima de 20 litros, desconto de 5% por litro
+   Gasolina - até 20 litros, desconto de 6% por litro
+   acima de 20 litros, desconto de 5% por litro
+Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90. 
+'''
+
+litros_vendidos = float(input("Digite a quantidade de litros vendidos: "))
+print(" A quantidade de litros vendida foi de ",litros_vendidos,"litros.")

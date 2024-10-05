6a5b7cfba877d4210ad7ead86f83ea185b51dc13
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-10-04 21:53:37 -0300

    Update exercicio_41.py

--- exercicio_41.py
+++ exercicio_41.py
@@ -1,8 +1,9 @@
 #41) Faça um algoritmo para ler as 3 notas obtidas por um aluno nas 3 verificações e a média dos exercícios que fazem parte da avaliação. Calcular a média de aproveitamento, usando a fórmula abaixo e escrever o conceito do aluno de acordo com a tabela de conceitos mais abaixo:
 
-#Média_de_aproveitamento = N1 + N2 * 2 + N3 * 3/7 
+#Média_de_aproveitamento = N1 + N2 * 2 + N3 * 3 /7 
 
 nota1 = float(input("Digite a primeira nota: ")) 
 nota2 = float(input("Digite a segunda nota: ")) 
 nota3 = float(input("Digite a terceira nota: ")) 
 
+media_aproveitamento = nota1 + (nota2 * 2 ) + (nota3 * 3 ) / 7

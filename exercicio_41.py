de396c65126d2759a6a9bed5644f2847d8fcfd3e
Cesar Menezes <alunocesar.menezes@gmail.com>
2024-10-11 20:15:57 -0300

    Update exercicio_41.py

--- exercicio_41.py
+++ exercicio_41.py
@@ -6,7 +6,7 @@ nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: ")) 
 nota3 = float(input("Digite a terceira nota: ")) 
 
-media_aproveitamento = nota1 + (nota2 * 2 ) + (nota3 * 3 ) / 7
+media_aproveitamento = (nota1 + (nota2 * 2 ) + (nota3 * 3 )) / 7
 print('a média é {:.2f}'.format(media_aproveitamento))
 
 if media_aproveitamento >=9:
